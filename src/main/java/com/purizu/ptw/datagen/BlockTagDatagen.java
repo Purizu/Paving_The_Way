6b43c37a185b72f932774b0af3af8fe84061cf69
ZETAMango <samyakchauhan02@gmail.com>
2025-11-30 00:46:01 +0530

    fixed an oopsie

--- src/main/java/com/purizu/ptw/datagen/BlockTagDatagen.java
+++ src/main/java/com/purizu/ptw/datagen/BlockTagDatagen.java
@@ -4,6 +4,7 @@ import com.purizu.ptw.contents.blocks.PtwBlocks;
 import com.purizu.ptw.contents.utils.PtwTags;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import net.minecraft.block.Blocks;
 import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.registry.tag.BlockTags;
 
@@ -39,21 +40,20 @@ public class BlockTagDatagen extends FabricTagProvider.BlockTagProvider{
                 .add(SOUL_SAND_PATH)
                 .add(SOUL_SOIL_PATH);
 
-        getOrCreateTagBuilder(PtwTags.Blocks.PATHABLE_BLOCKS_PICKAXE).add(NETHERRACK_PATH);
+        getOrCreateTagBuilder(PtwTags.Blocks.PATHABLE_BLOCKS_PICKAXE).add(Blocks.NETHERRACK);
 
         getOrCreateTagBuilder(PtwTags.Blocks.PATHABLE_BLOCKS)
-                .add(SHAVED_DIRT_PATH)
-                .add(COARSE_DIRT_PATH)
-                .add(MYCELIUM_PATH)
-                .add(PODZOL_PATH)
-                .add(MUD_PATH)
-                .add(SAND_PATH)
-                .add(RED_SAND_PATH)
-                .add(GRAVEL_PATH)
-                .add(CRIMSON_NYLIUM_PATH)
-                .add(WARPED_NYLIUM_PATH)
-                .add(SOUL_SAND_PATH)
-                .add(SOUL_SOIL_PATH);
-
+                .add(Blocks.DIRT)
+                .add(Blocks.COARSE_DIRT)
+                .add(Blocks.MYCELIUM)
+                .add(Blocks.PODZOL)
+                .add(Blocks.MUD)
+                .add(Blocks.SAND)
+                .add(Blocks.RED_SAND)
+                .add(Blocks.GRAVEL)
+                .add(Blocks.CRIMSON_NYLIUM)
+                .add(Blocks.WARPED_NYLIUM)
+                .add(Blocks.SOUL_SAND)
+                .add(Blocks.SOUL_SOIL);
     }
 }
